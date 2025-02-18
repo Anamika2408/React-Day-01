0e73bbf8d9c3c4815d986db975d06889fd1d0a27
Anamika Sharma <anamikasharma242001@gmail.com>
2025-02-18 15:40:29 +0530

    git commit -m "Initial Commit"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionalComponent from './Components/FunctionalComponent';
+import ClassComponent from './Components/ClassComponent';
 
+
+//comment
 function App() {
+  const element = <h1>Hello, World!</h1>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <FunctionalComponent/>
+      <ClassComponent/>
+      {element} 
     </div>
   );
 }
